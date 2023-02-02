35e4cfd0be90cc14eace86947c943e1665c152a8
stephonx <stephonh@yieldx.app>
2023-02-02 15:28:36 -0500

    added __init__ file and setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = 'roverpy', 
+    version = '0.1.0', 
+    description="Set of utility functions and classes to interact with rover sdk", 
+    author="Stephon Henry-Rerrie", 
+    author_email="stephonh@yieldx.app", 
+    packages=['roverpy']
+)
